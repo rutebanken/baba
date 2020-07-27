a9e038a75b421eba30d6a46c0c2583a19aa3bbd6
vincent <vincent.paturet@entur.org>
2020-07-27 16:34:32 +0200

    Code cleanup

--- src/main/java/no/rutebanken/baba/provider/repository/JpaProviderRepository.java
+++ src/main/java/no/rutebanken/baba/provider/repository/JpaProviderRepository.java
@@ -35,7 +35,7 @@ public class JpaProviderRepository implements ProviderRepository {
 
     @Override
     public Collection<Provider> getProviders() {
-        return this.entityManager.createQuery("SELECT p FROM Provider p order by p.id", Provider.class).setHint("org.hibernate.cacheable", Boolean.TRUE).getResultList();
+        return entityManager.createQuery("SELECT p FROM Provider p order by p.id", Provider.class).setHint("org.hibernate.cacheable", Boolean.TRUE).getResultList();
     }
 
     @Override
