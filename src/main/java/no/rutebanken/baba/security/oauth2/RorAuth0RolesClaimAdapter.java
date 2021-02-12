113c3342e1c05490c01d75bc609c2ca95ba5f091
vincent <vincent.paturet@entur.org>
2021-02-12 16:40:16 +0100

    migration to Auth0

--- src/main/java/no/rutebanken/baba/security/oauth2/RorAuth0RolesClaimAdapter.java
+++ src/main/java/no/rutebanken/baba/security/oauth2/RorAuth0RolesClaimAdapter.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 
 /**
- * Insert a "roles" and "realm_access" claims in the JWT token based on the namespaced "roles" and "role_assignments" claims, for compatibility with the existing
+ * Insert a "roles" and "realm_access" claims in the JWT token based on the "permission" claim and the  namespaced "roles" and "role_assignments" claims, for compatibility with the existing
  * authorization process (@{@link JwtRoleAssignmentExtractor}).
  */
 @Component
@@ -19,18 +19,26 @@ public class RorAuth0RolesClaimAdapter implements Converter<Map<String, Object>,
     private final MappedJwtClaimSetConverter delegate =
             MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
 
-    @Value("${baba.oauth2.resourceserver.auth0.ror.claim.namespace:https://ror.api.dev.entur.io/claims/}")
+    @Value("${baba.oauth2.resourceserver.auth0.ror.claim.namespace}")
     private String rorAuth0ClaimNamespace;
 
     @Override
     public Map<String, Object> convert(Map<String, Object> claims) {
         Map<String, Object> convertedClaims = this.delegate.convert(claims);
 
-        Object roles = convertedClaims.get(rorAuth0ClaimNamespace + "role");
+        // roles assigned to a user are found in the namespaced "roles" claim
+        Object roles = convertedClaims.get(rorAuth0ClaimNamespace + "roles");
         if (roles != null) {
             convertedClaims.put("realm_access", Map.of("roles", roles));
+        } else {
+            // roles assigned to an application (machine-to-machine service call) are found in the "permissions" claim
+            Object permissions = convertedClaims.get("permissions");
+            if (permissions != null) {
+                convertedClaims.put("realm_access", Map.of("roles", permissions));
+            }
         }
 
+        // role assignments for users and applications are found in the namespaced "role_assignments" claim
         Object roleAssignments = convertedClaims.get(rorAuth0ClaimNamespace + "role_assignments");
         if (roleAssignments != null) {
             convertedClaims.put("roles", roleAssignments);
