4e0f50d816a0fe19457a0017c4b3bf1bf4a606a5
vincent <vincent.paturet@entur.org>
2021-07-29 10:21:35 +0200

    Code cleanup

--- src/main/java/no/rutebanken/baba/organisation/repository/ResponsibilitySetRepositoryImpl.java
+++ src/main/java/no/rutebanken/baba/organisation/repository/ResponsibilitySetRepositoryImpl.java
@@ -19,23 +19,28 @@ package no.rutebanken.baba.organisation.repository;
 import no.rutebanken.baba.organisation.model.VersionedEntity;
 import no.rutebanken.baba.organisation.model.responsibility.ResponsibilitySet;
 import no.rutebanken.baba.organisation.model.responsibility.Role;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class ResponsibilitySetRepositoryImpl implements ResponsibilitySetRepositoryCustom {
-    @Autowired
-    private EntityManager entityManager;
+
+    private final EntityManager entityManager;
+
+    public ResponsibilitySetRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public List<ResponsibilitySet> getResponsibilitySetsReferringTo(VersionedEntity entity) {
 
         if (entity instanceof Role) {
             return entityManager.createQuery("select rs from ResponsibilitySet rs inner join rs.roles r" +
-                                                     " where r.typeOfResponsibilityRole=:role", ResponsibilitySet.class)
-                           .setParameter("role", entity).getResultList();
+                    " where r.typeOfResponsibilityRole=:role", ResponsibilitySet.class)
+                    .setParameter("role", entity).getResultList();
         }
 
         // TODO hanle other entity types
