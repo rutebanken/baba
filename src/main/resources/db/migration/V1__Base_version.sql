cebe7892dfa5f7419e3e1f4b798d3cbe55ce65d2
vpaturet <46598384+vpaturet@users.noreply.github.com>
2023-03-03 12:19:11 +0100

    Fix database initialization script (#270)

--- src/main/resources/db/migration/V1__Base_version.sql
+++ src/main/resources/db/migration/V1__Base_version.sql
@@ -1046,16 +1046,6 @@ ALTER TABLE ONLY user_account
 
 
 --
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
 -- PostgreSQL database dump complete
 --
 
