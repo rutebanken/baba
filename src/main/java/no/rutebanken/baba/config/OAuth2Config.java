ef1049ddcf366c3f576b1c4b698d179389cde0cf
Vincent Paturet <vincent.paturet@entur.org>
2024-03-18 06:52:22 +0100

    Add support for Entur Partner issuer

--- src/main/java/no/rutebanken/baba/config/OAuth2Config.java
+++ src/main/java/no/rutebanken/baba/config/OAuth2Config.java
@@ -17,9 +17,13 @@
 package no.rutebanken.baba.config;
 
 import org.entur.oauth2.JwtRoleAssignmentExtractor;
+import org.entur.oauth2.multiissuer.MultiIssuerAuthenticationManagerResolver;
+import org.entur.oauth2.multiissuer.MultiIssuerAuthenticationManagerResolverBuilder;
 import org.rutebanken.helper.organisation.RoleAssignmentExtractor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Configure Spring Beans for OAuth2 resource server and OAuth2 client security.
@@ -37,6 +41,34 @@ public class OAuth2Config {
     }
 
 
+    @Bean
+    @Profile("!test")
+    public MultiIssuerAuthenticationManagerResolver multiIssuerAuthenticationManagerResolver(
+            @Value("${baba.oauth2.resourceserver.auth0.entur.internal.jwt.audience:}")
+            String enturInternalAuth0Audience,
+            @Value("${baba.oauth2.resourceserver.auth0.entur.internal.jwt.issuer-uri:}")
+            String enturInternalAuth0Issuer,
+            @Value("${baba.oauth2.resourceserver.auth0.entur.partner.jwt.audience:}")
+            String enturPartnerAuth0Audience,
+            @Value("${baba.oauth2.resourceserver.auth0.entur.partner.jwt.issuer-uri:}")
+            String enturPartnerAuth0Issuer,
+            @Value("${baba.oauth2.resourceserver.auth0.ror.jwt.audience:}")
+            String rorAuth0Audience,
+            @Value("${baba.oauth2.resourceserver.auth0.ror.jwt.issuer-uri:}")
+            String rorAuth0Issuer,
+            @Value("${baba.oauth2.resourceserver.auth0.ror.claim.namespace:}")
+            String rorAuth0ClaimNamespace) {
+
+        return new MultiIssuerAuthenticationManagerResolverBuilder()
+                .withEnturInternalAuth0Issuer(enturInternalAuth0Issuer)
+                .withEnturInternalAuth0Audience(enturInternalAuth0Audience)
+                .withEnturPartnerAuth0Issuer(enturPartnerAuth0Issuer)
+                .withEnturPartnerAuth0Audience(enturPartnerAuth0Audience)
+                .withRorAuth0Issuer(rorAuth0Issuer)
+                .withRorAuth0Audience(rorAuth0Audience)
+                .withRorAuth0ClaimNamespace(rorAuth0ClaimNamespace)
+                .build();
+    }
 }
 
 
