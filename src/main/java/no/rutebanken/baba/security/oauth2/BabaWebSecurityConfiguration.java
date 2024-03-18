7cad97902dced3b0da6a9bd43190ab6e1bcb3395
Vincent Paturet <46598384+vpaturet@users.noreply.github.com>
2024-03-18 08:07:43 +0100

    Add support for Entur Partner issuer (#436)

--- src/main/java/no/rutebanken/baba/security/oauth2/BabaWebSecurityConfiguration.java
+++ src/main/java/no/rutebanken/baba/security/oauth2/BabaWebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package no.rutebanken.baba.security.oauth2;
 
+import org.entur.oauth2.multiissuer.MultiIssuerAuthenticationManagerResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
